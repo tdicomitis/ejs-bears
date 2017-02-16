99cfcf09be8c84ea634a36c801d030128123c347
Your Name <dicomitis13@gmail.com>
2017-02-16 14:19:03 -0700

    gave an ejs page access to our bears data

--- server.js
+++ server.js
@@ -3,24 +3,32 @@ var app = express();
 var bodyParser = require('body-parser');
 var bearRouter = require('./routes/bears');
 var catRouter = require('./routes/cats');
-
+var Bear = require('./models/bear')
 
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/mis-ejs-bears");
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index', {name: "Taryn"});
 });
-app.get('/view', function(req, res){
-  res.render('view');
-});
+
 app.get('/post', function(req, res){
   res.render('post');
+});
+
+app.get('/view', function(req, res){
+  Bear.find(function(err, allBears){
+    if(err){
+      console.log(err)
+    } else {
+      res.render('view', {bears: allBears})
+    }
+  });
 });
 
 app.use('/api/bears', bearRouter);
